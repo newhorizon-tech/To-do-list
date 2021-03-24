25962b452e761285bdebbe602663612897678fea
zerradi <dasileker@gmail.com>
2021-03-24 11:15:59 +0100

    refactored the import to default export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.scss';
 
 import projectFactory from './factory';
 import { initialize, saveData } from './storagedata';
-import { displayProjects } from './display'; // eslint-disable-line
+import displayProjects from './display'; // eslint-disable-line
 
 const initialData = initialize([], 0);
 
